d3a25b6e9022ca2c6dc3ff8e7d6d446d6abf9e6a
coding-zero-I <yashsangwan4700@gmail.com>
2025-12-17 10:25:50 +0530

    cors setup and webhook route definition

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello via Bun!");
+import express from "express";
+import userRouter from "./routes/userRoutes";
+import connectDB from "./config/dbConfig";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "HEAD", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.use("/api/v1/user", userRouter);
+
+app.listen(PORT, () => {
+  if (!process.env.MONGO_URI) {
+    console.error("MONGO_URI is not defined in environment variables");
+    process.exit(1);
+  }
+  connectDB(process.env.MONGO_URI);
+  console.log(`Server is running on port ${PORT}`);
+});
